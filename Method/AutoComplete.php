d749dcfca17b033608fd1957a3e2d9ddbb39dbef
Payam Naderi <naderi.payam@gmail.com>
2016-07-16 17:09:30 +0430

    initialize Client Service

--- Method/AutoComplete.php
+++ Method/AutoComplete.php
@@ -6,7 +6,25 @@ use Poirot\ApiClient\Request\Method;
 class AutoComplete 
     extends Method
 {
+    /** @link https://developers.google.com/places/web-service/autocomplete#place_types */
+    const TYPE_GEOCODE = 'geocode';
+    const TYPE_ADDRESS = 'address';
+    const TYPE_ESTABLISHMENT = 'establishment';
+    const TYPE_REGIONS = 'regions';
+    const TYPE_CITIES = 'cities';
+
+
+    protected $input;
+    protected $types;
+    protected $location;
+    protected $radius;
+    protected $offset;
+    protected $language;
+    protected $components = array();
+
     /**
+     * @override restrict method name change
+     *
      * Set Method Name
      *
      * @param string $method Method Name
@@ -20,6 +38,8 @@ class AutoComplete
     }
 
     /**
+     * @override restrict method name change
+     *
      * Get Method Name
      *
      * @return string
@@ -29,5 +49,199 @@ class AutoComplete
         return 'autocomplete';
     }
 
-    
+    /**
+     * @override options as arguments
+     *
+     * Get Method Arguments
+     *
+     * - we can define default arguments with some
+     *   values
+     *
+     * @return array
+     */
+    function getArguments()
+    {
+        $args = $this->args;
+
+        $merged = array();
+
+        $types      = $this->getTypes();
+        $location   = $this->getLocation();
+        $radius     = $this->getRadius();
+        $offset     = $this->getOffset();
+        $language   = $this->getLanguage();
+        $components = $this->getComponents();
+
+        ($types    === null)   ?: $merged['types']    = $types;
+        ($location === null)   ?: $merged['location'] = $location;
+        ($radius   === null)   ?: $merged['radius']   = $radius;
+        ($offset   === null)   ?: $merged['offset']   = $offset;
+        ($language === null)   ?: $merged['language'] = $language;
+        ($components === null) ?: $merged['components'] = $components;
+
+        $args = array_merge($args, $merged);
+        return $args;
+    }
+
+    // options:
+
+    /**
+     * The text string on which to search.
+     * The Place Autocomplete service will return candidate-
+     * matches based on this string and order results based-
+     * on their perceived relevance
+     *
+     * @param string $input
+     * @return $this
+     */
+    function setInput($input)
+    {
+        $input = (string) $input;
+        if ($input == '')
+            throw new \InvalidArgumentException('Input string is empty.');
+
+        $this->input = $input;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    function getInput()
+    {
+        return $this->input;
+    }
+
+    /**
+     * The types of place results to return.
+     * if no type is specified, all types will be returned.
+     *
+     * @param string $types
+     *
+     * @return $this
+     */
+    function setTypes($types)
+    {
+        $this->types = (string) $types;
+        return $this;
+    }
+
+    /**
+     * Get Types
+     * @return array
+     */
+    function getTypes()
+    {
+        return $this->types;
+    }
+
+    /**
+     * The point around which you wish to retrieve place information.
+     * must be specified as [latitude, longitude]
+     *
+     * @param array $location
+     *
+     * @return $this
+     */
+    function setLocation(array $location)
+    {
+        $this->location = $location;
+        return $this;
+    }
+
+    /**
+     * Get Location as [latitude, longitude]
+     * @return array
+     */
+    function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * The distance (in meters) within which to return place results.
+     *
+     * @param int $radius
+     *
+     * @return $this
+     */
+    function setRadius($radius)
+    {
+        $this->radius = $radius;
+        return $this;
+    }
+
+    /**
+     * Get Radius
+     * @return int|null
+     */
+    function getRadius()
+    {
+        return $this->radius;
+    }
+
+    /**
+     * The input term is 'Google abc' and the offset is 3,
+     * the service will attempt to match against 'Goo abc'.
+     *
+     * @param int $offset
+     *
+     * @return $this
+     */
+    function setOffset($offset)
+    {
+        $this->offset = $offset;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     * The language code, indicating in which language
+     * the results should be returned, if possible.
+     * @link https://developers.google.com/maps/faq#languagesupport
+     *
+     * @param string $language
+     *
+     * @return $this
+     */
+    function setLanguage($language)
+    {
+        $this->language = (string) $language;
+        return $this;
+    }
+
+    /**
+     * Get Language
+     * @return string|null
+     */
+    function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param array $components
+     * @return $this
+     */
+    function setComponents(array $components)
+    {
+        $this->components = array_merge($this->components, $components);
+        return $this;
+    }
+
+    /**
+     * Get Components
+     * @return array
+     */
+    function getComponents()
+    {
+        return $this->components;
+    }
 }
--- Places.php
+++ Places.php
@@ -4,6 +4,7 @@ namespace Poirot\GooglePlaceClient;
 use Poirot\ApiClient\aClient;
 use Poirot\ApiClient\Interfaces\iPlatform;
 
+use Poirot\ApiClient\Interfaces\Response\iResponse;
 use Poirot\Connection\Http\ConnectionHttpSocket;
 use Poirot\Connection\Interfaces\iConnection;
 
@@ -32,6 +33,54 @@ class Places
             $this->optsData()->import($options);
     }
 
+    // ...
+
+    /**
+     * Get place predictions.
+     *
+     * The service can be used to provide autocomplete functionality
+     * for text-based geographic searches, by returning places such
+     * as businesses, addresses and points of interest as a user types.
+     *
+     * @param string       $input    The text string on which to search
+     * @param string       $types    The types of place results to return.
+     *                               if no type is specified, all types will be returned.
+     *                               in general only a single type is allowed.
+     * @param array        $location The point around which you wish to retrieve place information.
+     *                               must be specified as [latitude, longitude]
+     * @param int          $radius   The distance (in meters) within which to return place results.
+     * @param int          $offset   The input term is 'Google abc' and the offset is 3,
+     *                               the service will attempt to match against 'Goo abc'.
+     *                               if no offset is supplied, the service will use the whole term.
+     * @param string       $language The language code, indicating in which language
+     *                               the results should be returned, if possible.
+     *                               @link https://developers.google.com/maps/faq#languagesupport
+     * @param string       $country The country must be passed as a two character,
+     *                              ISO 3166-1 Alpha-2 compatible country code.
+     *                              exp. fr
+     *
+     * @return iResponse
+     */
+    function getAutocomplete($input
+        , $types = null
+        , $location = null
+        , $radius = null
+        , $offset = null
+        , $language = null
+        , $country = null
+    ) {
+        $method = new AutoComplete();
+        $method->setInput($input);
+        
+        ($types    === null) ?: $method->setTypes($types);
+        ($location === null) ?: $method->setLocation($location);
+        ($radius   === null) ?: $method->setRadius($radius);
+        ($offset   === null) ?: $method->setOffset($offset);
+        ($language === null) ?: $method->setLanguage($language);
+        ($country  === null) ?: $method->setComponents(array('country' => (string) $country));
+        
+        return $this->call($method);
+    }
 
     // ...
 
